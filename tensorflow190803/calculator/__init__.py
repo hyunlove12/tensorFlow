22fe136d3dc668c82a8473c7d0c562ffd40760b6
dh <hyunlove12@naver.com>
2019-08-03 16:04:13 +0900

    no.3

--- tensorflow190803/calculator/__init__.py
+++ tensorflow190803/calculator/__init__.py
@@ -1,4 +1,13 @@
-from calculator.model import CalculatorModel
+from calculator.controller import CalculatorController
 #패키지  + 클래스
+'''
 if __name__ == '__main__':
-    pass
+    num1 = int(input('첫번째 수 \n'))
+    calc = CalculatorModel(2,3)
+    result = calc.add(int(num1),6)
+    print('계산결과 %d' % result)
+'''
+if __name__ == '__main__':
+    calc = CalculatorController()
+    calc.execute()
+
--- tensorflow190803/calculator/controller.py
+++ tensorflow190803/calculator/controller.py
@@ -1,3 +1,22 @@
+from calculator.model import CalculatorModel
+
 class CalculatorController:
     def __init__(self):
-        pass
+        self.num1 = int(input('숫자를  입력하세요 \n'))
+        self.num2 = int(input('숫자를  입력하세요 \n'))
+        self.op = input('연산자를 입력하세요 \n')
+        self.calc = CalculatorModel(self.num1, self.num2)
+
+    def execute(self):
+        if self.op == '+':
+            result = self.calc.add(self.num1,self.num2)
+            print(result)
+        elif self.op == '-':
+            result = self.calc.sub(self.num1, self.num2)
+            print(result)
+        elif self.op == '*':
+            result = self.calc.mul(self.num1, self.num2)
+            print(result)
+        elif self.op == '/':
+            result = self.calc.div(self.num1, self.num2)
+            print(result)
